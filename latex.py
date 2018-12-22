79d7f5b08535819ea3e7550aad4cd45d8195e5e8
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2018-12-22 11:15:19 +0100

    Update latex.py

--- latex.py
+++ latex.py
@@ -17,18 +17,20 @@ class SevenZip(object):
 	def SevenZIP(self):
 		if(self.files_7z.endswith(".7z") == True):
 			with open(self.wordlist, "r") as file:
-				for file_output in file.readlines():
-					# Create loop for bruteforce ! :D
-					file_output = file_output.strip("\n")
-					send_cmd = "7z x -p%s %s -aoa >/dev/null" %(file_output, self.files_7z)
-					# Creating command for sending ! :D
-					BertModel = command_execution.system(send_cmd)
+				file = file.readlines()
+				# Read files ! :)
+			for file_output in file:
+				# Create loop for bruteforce ! :D
+				file_output = file_output.strip("\n")
+				send_cmd = "7z x -p%s %s -aoa >/dev/null" %(file_output, self.files_7z)
+				# Creating command for sending ! :D
+				BertModel = command_execution.system(send_cmd)
 
-					if not(BertModel == 0):
-						print "[-] Password not found sorry : %s" %(file_output)
-					else:
-						print "\n[+] Password 7z cracked with success : %s\n" %(file_output)
-						sys.exit(0)
+				if not(BertModel == 0):
+					print "[-] Password not found sorry : %s" %(file_output)
+				else:
+					print "\n[+] Password 7z cracked with success : %s\n" %(file_output)
+					sys.exit(0)
 
 		elif not(self.files_7z.endswith(".7z")):
 			raise SevenZipIncorrect("Error file extension")
