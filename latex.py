2b89cb3418bd17e0e88f7867c95e8326674418e2
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2018-12-30 09:35:51 +0100

    Update latex.py

--- latex.py
+++ latex.py
@@ -1,33 +1,70 @@
 #coding:utf-8
 
 import sys
-import os
+import os as subprocessing
 import Queue
 import threading
 import optparse
-import py7zlib
 
 class SevenZipIncorrect(Exception):
 	def __init__(self, error_zip):
 		self.error_zip = error_zip
 
 class SevenZip(threading.Thread):
-	def __init__(self, threads=35):
+	def __init__(self, threads=35, command=None):
 		threading.Thread.__init__(self)
 		# System of Threads is present
 		self.threads_tds = threads
+		self.command_tds = command
+
 		self.argument_on = sys.argv[1]
 		self.argument_wd = sys.argv[2]
 
 	def ExtensionModel(self, q):
+		"""
+		This function will attack
+		the file 7z is to hack the file
+
+		Parameters
+		----
+			self : They are supervariables or they can communicate anywhere
+			q = The variable q is the wordlist is called
+
+		Returns
+		----
+			This function will return 
+			the attack and password cracker
+		"""
 		if(self.argument_on.endswith(".7z") == True):
 			while True:
 				BertModel = q
 				BertModel = BertModel.get()
+				# Execution command for bruteforce ! :D
+				self.command_tds = ("7z x -p%s %s -aoa >/dev/null" %(BertModel, self.argument_on))
+				output_status_ts = subprocessing.system(self.command_tds)
+				# Starting bruteforce ! :D
+				if(output_status_ts == 0):
+					print "\n[+] Password cracked with success : %s\n" %(BertModel)
+					sys.exit(1)
+				else:
+					print "[-] Password not cracked : %s" %(BertModel)
+
 		else: # exceptions error
 			raise SevenZipIncorrect("ExtensionFileIncorrect")
 
 	def run(self):
+		"""
+		This function will optimize the 
+		program by boosting it with threads
+
+		Parameters
+		----
+			self : They are supervariables or they can communicate anywhere
+	
+		Returns
+		----
+			This function will not return much
+		"""
 		q = Queue.Queue()
 		with open(self.argument_wd, "r") as BertModel:
 			for Queue_Reverse in BertModel:
