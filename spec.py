8f3c9f8c76d0f357e6a78f527c4d598f230d98fe
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2018-12-31 21:23:38 +0100

    Update spec.py

--- spec.py
+++ spec.py
@@ -11,15 +11,15 @@ if(CHECK_VERSION):
 
 class KalBinary(object):
 	def __init__(self,
-				algorithms_params=None,
 				CalculationBytes=None,
 				CalculationOctet=None,
-				hash_dec=None):
+				ArgumentationFal=None,
+				AlphabetEu=None):
 
-		self.algorithms = algorithms_params
 		self.CalculationBytes = CalculationBytes
 		self.CalculationOctet = CalculationOctet
-		self.hash_dec = hash_dec
+		self.ArgumentationFal = ArgumentationFal
+		self.AlphabetEu = AlphabetEu
 		self.encrypt = argument.encrypt
 
 	def BertModel(self, octet_algorithm):
@@ -49,24 +49,100 @@ class KalBinary(object):
 				SecondValue = int(self.CalculationOctet / self.CalculationBytes)
 
 				if FirstValue == int(octet_algorithm):
-					self.hash_dec = "%s/%s=" % (str(self.CalculationBytes), str(self.CalculationOctet))
+					self.ArgumentationFal = "%s/%s=" % (str(self.CalculationBytes), str(self.CalculationOctet))
 				if SecondValue == int(octet_algorithm):
-					self.hash_dec = "%s/%s=" % (str(self.CalculationOctet), str(self.CalculationBytes))
+					self.ArgumentationFal = "%s/%s=" % (str(self.CalculationOctet), str(self.CalculationBytes))
 				
 				# We are in a condition that will test specific variables like the variable #FirstValue #SecondValue
 				# Variables that behave in a way with bytes.
 
 				if AddModel == int(octet_algorithm):
-					self.hash_dec = "%s+%s=" % (str(self.CalculationBytes), str(self.CalculationOctet))
+					self.ArgumentationFal = "%s+%s=" % (str(self.CalculationBytes), str(self.CalculationOctet))
 				if SubModel == int(octet_algorithm):
-					self.hash_dec = "%s-%s=" % (str(self.CalculationBytes), str(self.CalculationOctet))
+					self.ArgumentationFal = "%s-%s=" % (str(self.CalculationBytes), str(self.CalculationOctet))
 
 				# We do not have supervariables like for example with the selfs or that kind of thing.
 				# But it's always good to have the selfs to communicate the variables everywhere.
 
 				if Sub_Model == int(octet_algorithm):
-					self.hash_dec = "%s-%s=" % (str(self.CalculationOctet), str(self.CalculationBytes))
+					self.ArgumentationFal = "%s-%s=" % (str(self.CalculationOctet), str(self.CalculationBytes))
 				if MulModel == int(octet_algorithm):
-					self.hash_dec = "%s*%s=" % (str(self.CalculationBytes), str(self.CalculationOctet))
+					self.ArgumentationFal = "%s*%s=" % (str(self.CalculationBytes), str(self.CalculationOctet))
 
-		return self.hash_dec
+		return self.ArgumentationFal
+	
+	def BertPanel(self, BinaryArg):
+		"""
+		This function will handle
+		the data encryption.
+
+		Parameters
+		----
+		self : They are supervariables or they can communicate anywhere
+		octet_algorithm : The bytes of the variable
+
+		Returns
+		----
+		Returns octets with success and brow.
+		finished.
+		"""
+		self.AlphabetEu = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		self.AlphabetEu = list(self.AlphabetEu)
+	
+		ArgumentCrypt = ""
+	
+		for Carling in BinaryArg:
+			try:
+				# Create loop for hashing.
+				Carling = int(Carling)
+				NewUses = self.AlphabetEu[car]
+				ArgumentCrypt += NewUses
+				# Create loop for hashing.
+			except:
+				ArgumentCrypt += Carling
+			
+		return(ArgumentCrypt)
+	
+	def BertBinary(self, ArgumentTexte):
+		"""
+		This function will handle
+		the data encryption.
+
+		Parameters
+		----
+		self : They are supervariables or they can communicate anywhere
+		octet_algorithm : The bytes of the variable
+
+		Returns
+		----
+		Returns octets with success and brow.
+		finished.
+		"""
+		ArgumentOctets = [ bin(ord(ch))[2:].zfill(8) for ch in ArgumentTexte ]
+		ArgumentList = []
+	
+		for BytesArgument in ArgumentOctets:
+			SegmentOne = BytesArgument[:3]
+			SegmentTwo = BytesArgument[3:]
+		
+			ArgumentList.append(SegmentOne)
+			ArgumentList.append(SegmentTwo)
+		
+		return(ArgumentList)
+
+	def run(self): 
+
+		calc_text = ""
+
+		bin_list = self.BertBinary(ArgumentTexte)
+
+		for octet in bin_list:
+			calcul = self.BertModel(octet_algorithm)
+			calc_text += calcul
+
+		final_crypt = self.BertPanel(calc_text)
+		print("HASH : "+final_crypt)
+
+if __name__ == "__main__":
+	Algorithm = KalBinary()
+	Algorithm.run()	
