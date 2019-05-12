da59005966838476d9eecc0de1767c8ca4d50edb
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-05-12 17:23:53 +0200

    Create exceptions.py

---
+++ core/exceptions.py
@@ -0,0 +1,7 @@
+class SevenZipIncorrect(Exception):
+	def __init__(self, error_zip):
+		self.error_zip = error_zip
+
+class SevenZipNoInstall(Exception):
+	def __init__(self, error_dls):
+		self.error_dls = error_dls
