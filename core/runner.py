348f6da7ecd583655ea7b71679ac1b9342214ce1
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-05-13 17:44:37 +0200

    Update runner.py

--- core/runner.py
+++ core/runner.py
@@ -3,19 +3,33 @@
 
 import pzma
 import options
+import threading
 import exceptions
 
-class runner(object):
-    def __init__(self, output_condition=None, error_condition=None):
+class runner(threading.Thread):
+    def __init__(self, output_condition=None, error_condition=None, output_threading=35):
+        threading.Thread.__init__(self)
+        '''
+            create function for call after
+            the variable __init__().
+        '''
         self.output_condition = pzma.pzma_br()
         self.error_condition  = error_condition
+        self.output_threading = output_threading
 
     def __str__(self):
+        '''
+            This function is used to
+            manage program errors __str__()
+        '''
         if(self.output_condition.seven_which_exist() == False):
-            raise SevenZipNoInstall("7z is not installed in your computer.")
+            raise exceptions.SevenZipNoInstall("7z is not installed in your computer.")
 
         if(self.output_condition.seven_archive_exist(options.files) == False):
-            raise SevenZipNoFound("7z files is not found in your computer.")
+            raise exceptions.SevenZipNoFound("7z files is not found in your computer.")
 
         if(self.output_condition.seven_check_file(options.files) == False):
-            raise SevenZipIncorrect("7z, please select archive file .7z.")
+            raise exceptions.SevenZipIncorrect("7z, please select archive file .7z.")
+
+if __name__ == "__main__":
+    runner()
