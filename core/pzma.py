0b0b8e6d4d62715348a88878f67c2cbb8a48398e
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-05-13 16:08:33 +0200

    Update pzma.py

--- core/pzma.py
+++ core/pzma.py
@@ -30,41 +30,44 @@ if(sys.version_info >= (2, 0) and sys.version_info <= (3, 0)):
     if(return_p.system("which 7z >/dev/null") != 0):
         raise exceptions.SevenZipNoInstall("Please install 7z in your computer.")
 
-def seven_archive_exist(seven_archive):
-    '''
-        Lets you test if the 7z file exists in the computer system
-        in function for check __check_filenames__().
-    '''
-    try:
-        with open(seven_archive, "rb") as enumeration_file:
-            enumeration_file = True
-    except IOError as error_file_output:
-        enumeration_file = False
+class pzma_br(object):
+    def __init__(self, enumeration_file=None, property_c=" "):
+        self.enumeration_file = enumeration_file
+        self.property_c       = property_c
 
-    # The return value in function __seven_archive_exist()__,
-    # is exactly here my friends for calling after function().
+    def seven_archive_exist(seven_archive):
+        '''
+            Lets you test if the 7z file exists in the computer system
+            in function for check __check_filenames__().
+        '''
+        try:
+            with open(seven_archive, "rb") as self.enumeration_file:
+                self.enumeration_file = True
+        except IOError as error_file_output:
+            self.enumeration_file = False
 
-    return enumeration_file
+        # The return value in function __seven_archive_exist()__,
+        # is exactly here my friends for calling after function().
 
-def decompress_data(filename_archive, end_password):
-    '''
-       This function is used to decrypt
-       the file __decompress_data()__.
-    '''
-    property_c = " "
+        return self.enumeration_file
 
-    method_req  = __name__["DECOMPRESS"] + property_c
-    method_req += __name__["COMPRESS"] + property_c
-    method_req += __name__["DECOMPRESS_PASSWORD"] + end_password + property_c + options.files + property_c
-    method_req += __name__["COMPRESS_PASSWORD"] + property_c
-    method_req += __name__["BINARY_STRINGS"]
+    def decompress_data(filename_archive, end_password):
+        '''
+        This function is used to decrypt
+        the file __decompress_data()__.
+        '''
+        method_req  = __name__["DECOMPRESS"] + self.property_c
+        method_req += __name__["COMPRESS"] + self.property_c
+        method_req += __name__["DECOMPRESS_PASSWORD"] + end_password + self.property_c + options.files + self.property_c
+        method_req += __name__["COMPRESS_PASSWORD"] + self.property_c
+        method_req += __name__["BINARY_STRINGS"]
 
-    if(return_p.system(method_req) == 0):
-        password_bool = True
-    elif(return_p.system(method_req) != 0):
-        password_bool = False
+        if(return_p.system(method_req) == 0):
+            password_bool = True
+        elif(return_p.system(method_req) != 0):
+            password_bool = False
 
-    # So concretely it will return true or
-    # false if the password is wrong or not decompress_data()
+        # So concretely it will return true or
+        # false if the password is wrong or not decompress_data()
 
-    return password_bool
+        return password_bool
